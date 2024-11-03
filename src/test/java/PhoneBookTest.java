060738d60a229682150583b797b938efac4c5277
Evgeny Nikitin <ev0909@rambler.ru>
2024-11-03 15:09:40 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
 
+    @Test
+    public void testAdd() {
+        int result = PhoneBook.add("Petya", "89260000000") +
+                PhoneBook.add("Olya", "89260000001");
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void testAddName() {
+        int result = PhoneBook.add("Petya", "89260000000") +
+                PhoneBook.add("Olya", "89260000001")
+                + PhoneBook.add("Olya", "89260000001");
+        assertEquals(result, 5);
+    }
 }
